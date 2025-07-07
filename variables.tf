47b5c198bf685b9f0de85f9b5c032e9a408857cc
BharathKumarReddy <nbkumar2103@gmail.com>
2025-07-07 07:52:06 +0530

    updated .tf file

---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_id" {
+    default = "Z09266885JREMC64NO1J"
+}
+
+variable "zone_name" {
+    default = "bharath2103.site"
+}
+
+variable "component" {
+
+}
+
+variable "rule_priority" {
+    
+}
