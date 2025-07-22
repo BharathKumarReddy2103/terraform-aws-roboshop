229538d1f49bb65ecbe7537e21de32887a70a09f
BharathKumarReddy <nbkumar2103@gmail.com>
2025-07-22 06:06:18 +0530

    updated file

--- data.tf
+++ data.tf
@@ -18,7 +18,7 @@ data "aws_ssm_parameter" "frontend_alb_listener_arn" {
   name = "/${var.project}/${var.environment}/frontend_alb_listener_arn"
 }
 
-data "aws_ami" "joindevops" {
+data "aws_ami" "bharathdevops" {
   owners           = ["973714476881"]
   most_recent      = true
 
